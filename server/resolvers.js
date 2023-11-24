ecbfc033d82286578160e03e4620bb12c25b6b78
Demi-O <demiladeolagoke@yahoo.ca>
2023-11-24 16:20:41 -0300

    updated schema and resolvers to get job and company information from the DB

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,5 +1,17 @@
+import { getCompany } from './db/companies.js';
+import { getJobs } from './db/jobs.js';
+
+function toISODate(value) {
+  return value.slice(0, 'yyyy-mm-dd'.length);
+}
+
 export const resolvers = {
   Query: {
-    greeting: () => 'Hello World!',
+    jobs: () => getJobs(),
   },
+  
+  Job: {
+    date: (job) => toISODate(job.createdAt),
+    company: (job) => getCompany(job.companyId),
+  }
 };
