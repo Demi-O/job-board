04fea09942112389a9439db6afd9a8ecb4b7d358
Demi-O <demiladeolagoke@yahoo.ca>
2023-12-04 19:48:00 -0300

    added custom errors to server, added error handling with state management to company page on client

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,3 +1,4 @@
+import { GraphQLError } from 'graphql';
 import { getCompany } from './db/companies.js';
 import { getJob, getJobs, getJobsByCompany } from './db/jobs.js';
 
@@ -5,10 +6,24 @@ function toISODate(value) {
   return value.slice(0, 'yyyy-mm-dd'.length);
 }
 
+function notFoundError(message) {
+  return new GraphQLError(message, {
+    extensions: { code: 'NOT_FOUND' },
+  });
+}
+
 export const resolvers = {
   Query: {
-    company: (_root, { id }) => getCompany(id),
-    job: (_root, { id }) => getJob(id),
+    company: async (_root, { id }) => {
+      const company = await getCompany(id);
+      if (!company) return notFoundError(`No company found with id ${id}`);
+      return company;
+    },
+    job: async (_root, { id }) => {
+      const job = await getJob(id);
+      if (!job) return notFoundError(`No job found with id ${id}`);
+      return job;
+    },
     jobs: () => getJobs(),
   },
 
