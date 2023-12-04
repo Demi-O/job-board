a12a9ced0f5846a6d9c05ee6d1f9463969582412
Demi-O <demiladeolagoke@yahoo.ca>
2023-12-04 19:11:15 -0300

    updated client and server to get jobs and companies by ID

--- client/src/lib/graphql/queries.js
+++ client/src/lib/graphql/queries.js
@@ -2,6 +2,46 @@ import { GraphQLClient, gql } from 'graphql-request';
 
 const client = new GraphQLClient('http://localhost:9000/graphql');
 
+export async function getCompany(id) {
+  const query = gql`
+    query ($id: ID!) {
+      company(id: $id) {
+        id
+        name
+        description
+        jobs {
+          id
+          date
+          title      
+        }
+      }
+    }
+  `;
+
+  const { company } = await client.request(query, { id });
+  return company;
+}
+
+export async function getJob(id) {
+  const query = gql`
+    query ($id: ID!) {
+      job(id: $id) {
+        id
+        date
+        title
+        company {
+          id
+          name
+        }
+        description
+      }
+    }
+  `;
+
+  const { job } = await client.request(query, { id });
+  return job;
+};
+
 export async function getJobs() {
   const query = gql`
     query {
@@ -19,4 +59,4 @@ export async function getJobs() {
 
   const { jobs } = await client.request(query);
   return jobs;
-}
+};
--- server/resolvers.js
+++ server/resolvers.js
@@ -1,5 +1,5 @@
 import { getCompany } from './db/companies.js';
-import { getJob, getJobs } from './db/jobs.js';
+import { getJob, getJobs, getJobsByCompany } from './db/jobs.js';
 
 function toISODate(value) {
   return value.slice(0, 'yyyy-mm-dd'.length);
@@ -7,8 +7,13 @@ function toISODate(value) {
 
 export const resolvers = {
   Query: {
+    company: (_root, { id }) => getCompany(id),
     job: (_root, { id }) => getJob(id),
     jobs: () => getJobs(),
+  },
+
+  Company: {
+    jobs: (company) => getJobsByCompany(company.id),
   },
   
   Job: {
