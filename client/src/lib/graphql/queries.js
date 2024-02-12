e474fb2e4438b00c3b91ab67618bec3ed85400e5
Demi-O <demiladeolagoke@yahoo.ca>
2024-02-12 16:53:52 -0600

    added use of access token to website

--- client/src/lib/graphql/queries.js
+++ client/src/lib/graphql/queries.js
@@ -1,10 +1,20 @@
 import { GraphQLClient, gql } from 'graphql-request';
+import { getAccessToken } from '../auth';
 
-const client = new GraphQLClient('http://localhost:9000/graphql');
+const client = new GraphQLClient('http://localhost:9000/graphql', {
+  headers: () => {
+    const accessToken = getAccessToken()
+    if (accessToken) {
+      return { "Authorization": `Bearer ${accessToken}`}
+    }
+
+    return {}
+  }
+});
 
 export async function createJob({ title, description }) {
   const mutation = gql`
-    mutation CreateJob ($input: CreateJobInput) {
+    mutation CreateJob ($input: CreateJobInput!) {
       job: createJob(input: $input) {
         id
       }
