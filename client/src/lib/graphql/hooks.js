bb28f087ad1a4af170af529ef3a7965d307f3a55
Demi-O <demiladeolagoke@yahoo.ca>
2024-04-23 17:27:46 -0500

    moved create job mutation logic into createJob hook

--- client/src/lib/graphql/hooks.js
+++ client/src/lib/graphql/hooks.js
@@ -1,6 +1,6 @@
-import { useQuery } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 
-import { companyByIdQuery, getJobsQuery, jobByIdQuery } from "./queries";
+import { companyByIdQuery, createJobMutation, getJobsQuery, jobByIdQuery } from "./queries";
 
 export function useCompany(id) {
   const { data, error, loading } = useQuery(companyByIdQuery, {
@@ -24,4 +24,25 @@ export function useJobs() {
   });
 
   return { jobs: data?.jobs, loading, error: Boolean(error) };
+}
+
+export function useCreateJob() {
+  const [mutate, { loading }] = useMutation(createJobMutation);
+  const createJob = async (title, description) => {
+    const { data: { job } } = await mutate({
+      variables: { input: { title, description } },
+      update: (cache, result) => {
+        const { data } = result;
+        cache.writeQuery({
+          query: jobByIdQuery,
+          variables: { id: data.job.id },
+          data,
+        });
+      },
+    });
+
+    return job;
+  };
+
+  return { createJob, loading };
 }
